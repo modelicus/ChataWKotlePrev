639b8946c525dd11ebedd56f24304d5eb1f7ada5
modelicus <206447528+modelicus@users.noreply.github.com>
2025-10-12 19:47:57 +0200

    Added offer and invite sections. Refined styling.

--- script.js
+++ script.js
@@ -52,3 +52,92 @@ gsap.to(".aboutImage", {
         scrub: true
     }
 });
+
+gsap.to(".offerImage", {
+    y: "40%",
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".offerImgWrap",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true
+    }
+});
+
+const video = document.querySelector("#video-background video");
+let src = video.currentSrc || video.src;
+console.log(video, src);
+
+/* Make sure the video is 'activated' on iOS */
+function once(el, event, fn, opts) {
+    var onceFn = function (e) {
+        el.removeEventListener(event, onceFn);
+        fn.apply(this, arguments);
+    };
+    el.addEventListener(event, onceFn, opts);
+    return onceFn;
+}
+
+once(document.documentElement, "touchstart", function (e) {
+    video.play();
+    video.pause();
+});
+
+/* ---------------------------------- */
+/* Scroll Control! */
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+    defaults: { duration: 1 },
+    scrollTrigger: {
+        trigger: "#videoContainer",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: true
+    }
+});
+
+once(video, "loadedmetadata", () => {
+    tl.fromTo(
+        video,
+        {
+            currentTime: 0
+        },
+        {
+            currentTime: video.duration || 1
+        }
+    );
+});
+
+/* When first coded, the Blobbing was important to ensure the browser wasn't dropping previously played segments, but it doesn't seem to be a problem now. Possibly based on memory availability? */
+setTimeout(function () {
+    if (window["fetch"]) {
+        fetch(src)
+            .then((response) => response.blob())
+            .then((response) => {
+                var blobURL = URL.createObjectURL(response);
+
+                var t = video.currentTime;
+                once(document.documentElement, "touchstart", function (e) {
+                    video.play();
+                    video.pause();
+                });
+
+                video.setAttribute("src", blobURL);
+                video.currentTime = t + 0.01;
+            });
+    }
+}, 1000);
+
+gsap.to("#invite", {
+    opacity: 1,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: "#videoContainer",
+        start: "top",
+        end: "bottom bottom",
+        scrub: true
+    }
+});
